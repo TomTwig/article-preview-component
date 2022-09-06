9b4b4239eefd4b1be7ec6239d12bbdd052fc165f
Tom Rosenzweig <tom.rosenzweig@live.de>
2022-09-06 17:30:18 +0200

    add social mobile wrong approach

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 const arrowButton = document.getElementById("arrow");
-
 const socialMenu = document.getElementById("social-menu");
+const profile = document.getElementById("card__content__profile");
+const profileContainer = document.getElementById("card__content__profile__container");
 
-let showSocialMenu = false;
+// let showSocialMenu = false;
 
-arrowButton.addEventListener("click",() =>{
+// arrowButton.addEventListener("click",() =>{
 
-    if(showSocialMenu){
-        socialMenu.style.display = "none";
-        showSocialMenu = false;
+//     if(showSocialMenu){
+//         socialMenu.style.display = "none";
+//         showSocialMenu = false;
        
-    }else{
-        socialMenu.style.display = "flex";
-        showSocialMenu = true;
-    }
+//     }else{
+//         socialMenu.style.display = "flex";
+//         showSocialMenu = true;
+//     }
+
+// })
+
+arrowButton.addEventListener("click",()=>{
+
+    profile.remove();
+    arrowButton.remove();
+    socialMenu.style.display = "flex";
+    socialMenu.style.flexDirection= "space-between";
+    socialMenu.style.position = "unset";
+    socialMenu.style.width = "100%";
+
+    socialMenu.appendChild(arrowButton);
+
+
+     profileContainer.appendChild(socialMenu);
+
+
 
 })
+
+
