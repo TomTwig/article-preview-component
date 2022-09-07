65384b70ba47c6513a11b64da1809b1ae6172462
Tom Rosenzweig <tom.rosenzweig@live.de>
2022-09-07 16:46:25 +0200

    switch desktop mobile broken

--- main.js
+++ main.js
@@ -2,38 +2,60 @@ const arrowButton = document.getElementById("arrow");
 const socialMenu = document.getElementById("social-menu");
 const profile = document.getElementById("card__content__profile");
 const profileContainer = document.getElementById("card__content__profile__container");
+const link = document.getElementById("link");
 
-// let showSocialMenu = false;
-
-// arrowButton.addEventListener("click",() =>{
-
-//     if(showSocialMenu){
-//         socialMenu.style.display = "none";
-//         showSocialMenu = false;
-       
-//     }else{
-//         socialMenu.style.display = "flex";
-//         showSocialMenu = true;
-//     }
-
-// })
-
-arrowButton.addEventListener("click",()=>{
-
-    profile.remove();
-    arrowButton.remove();
-    socialMenu.style.display = "flex";
-    socialMenu.style.flexDirection= "space-between";
-    socialMenu.style.position = "unset";
-    socialMenu.style.width = "100%";
-
-    socialMenu.appendChild(arrowButton);
-
-
-     profileContainer.appendChild(socialMenu);
+let isShowSocialMenu = false;
 
 
 
-})
+
+arrowButton.addEventListener("click",() => {
+
+
+if(window.innerWidth >= 600){
+
+    link.firstChild = arrowButton;
+    if(isShowSocialMenu){
+        isShowSocialMenu = false;
+        socialMenu.style.display ="none";
+    }else{
+        isShowSocialMenu = true;
+        socialMenu.style.display = "flex";
+    }
+}else{
+
+    if(isShowSocialMenu){
+        isShowSocialMenu = false;
+        
+        
+        socialMenu.style.display ="none";
+        profileContainer.style.display ="flex";
+        profile.appendChild(arrowButton);
+        profile.style.justifyContent="center";
+        profile.style.display ="flex";
+    
+
+    }else{
+        socialMenu.style.display = "flex";
+        socialMenu.appendChild(arrowButton);
+        profile.style.display ="none";
+        profileContainer.style.display ="block";
+        isShowSocialMenu = true;
+    }
+
+    
+
+
+}
+
+
+   
+    })
+
+ 
+
+   
+
+
 
 
